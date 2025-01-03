e54029aff76ca130371d636d359a2c2cf8c28f14
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-01-03 14:26:51 +0530

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,23 @@
+const name = "hitesh";
+const repoCount = 50;
+
+console.log(name + ' ' + repoCount ,'value')
+
+const gameName = new String('shreyash-sj')
+
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt());
+
+const newString = gameName.substring(0,4)
+console.log(newString)
+
+const newStringOne = "          shreyash     "
+console.log(newStringOne.trim())
+
+const URL = "https://shreyash.com/shreyash%20jadhav";
+
+console.log(URL.replace('%20', '-'));
+
+console.log(gameName.split('-'));
+
