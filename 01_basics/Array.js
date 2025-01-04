e2795f57594e4a668c3b9a36c0c8d178530dac8c
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-01-04 10:44:42 +0530

    array.js

---
+++ 01_basics/Array.js
@@ -0,0 +1,16 @@
+// Arrays 
+
+let myArray = [1, 2, 3, 4, 5];
+// console.log(myArray);
+let newArr = myArray.join();
+
+
+// console.log(newArr);
+// console.log(typeof newArr);
+
+myArray.push(6)
+console.log(myArray);
+
+myArray.pop()
+console.log(14,myArray);
+
