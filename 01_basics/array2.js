2f867a96b690177f537b99f41384d1c8c9d0abc4
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-01-04 10:45:23 +0530

    array-ii.js

---
+++ 01_basics/array2.js
@@ -0,0 +1,8 @@
+
+
+const marval_heros = ['thoer', 'ironman','spiderman']
+
+const dc_heros = ['superman', 'flash', 'batman']
+
+// let allHeros =  marval_heros.join(dc_heros)
+console.log(marval_heros.push(dc_heros))
