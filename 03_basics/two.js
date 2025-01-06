9dfbc2ecd155191f737c1219c4712b217e12bf05
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-01-06 16:52:15 +0530

    two.js

---
+++ 03_basics/two.js
@@ -0,0 +1,25 @@
+// switch (key) {
+//     case value:
+//         break;
+//     default:
+//         break;
+// }
+
+
+let month = 2
+
+switch (month) {
+    case 1:
+        console.log('jan');
+        break;
+    case 2:
+        console.log('feb');
+        break;
+    case 3: 
+        console.log('mar');
+        break;
+    default:
+        console.log('default case match');
+        
+        break;
+}   
