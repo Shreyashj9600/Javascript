6ee6484904437021c1b5632d44068e961b714731
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-01-06 16:51:36 +0530

    one.js

---
+++ 03_basics/one.js
@@ -0,0 +1,15 @@
+//Control flow in javascript
+
+if (true) {
+    console.log('hello')
+}
+''
+let balance = 500;
+
+if (balance < 750) {
+    console.log('balance is less tahn 750')
+} else if (balance < 900) {
+    console.log('balance is less than 900')
+} else if (balance < 1200){
+    console.log('balance is less than 1000')
+}
