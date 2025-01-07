e8a7c6f77462e61dbe5fdcefeabe1ac4d1c77727
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-01-07 09:28:06 +0530

    forLoop.js

---
+++ 04_loop/one.js
@@ -0,0 +1,17 @@
+// for loop
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+// }
+
+for (let index = 0; index < 10 ; index++) {
+    const element = index;
+    console.log(element)
+}
+
+let myArr = [1,2,3,4,5,6,7,8,9,0]
+
+for(let i = 0; i < myArr.length; i++){
+    console.log('This is my value' , myArr[i]);
+    
+}
