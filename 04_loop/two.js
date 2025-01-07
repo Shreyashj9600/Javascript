d3940e59ebebf6a153d58a487163884e38f02511
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-01-07 14:02:33 +0530

    two.js

---
+++ 04_loop/two.js
@@ -0,0 +1,24 @@
+// While and do while loop 
+
+index = 0;
+while(index <= 10){
+    console.log(`value of index is ${index}`)
+    index = index + 2;
+}
+
+let myArr = ['flash','batman','superman']
+
+let arr = 0
+while(arr < myArr.length){
+    console.log(`Array element is ${myArr[arr]}`)
+    arr = arr + 1
+}
+
+// do while
+
+
+let score = 1
+do{
+    console.log(`value of score is ${score}`)
+    score = score + 1
+}while(score <= 10)
